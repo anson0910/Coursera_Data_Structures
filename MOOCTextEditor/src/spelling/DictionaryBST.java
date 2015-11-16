b9846945d607a7f24887ffa6349aff7ed16be172
Anson <anson32330200@gmail.com>
2015-11-16 17:16:03 +0800

    Finish simple BST implementation of dictionary

--- MOOCTextEditor/src/spelling/DictionaryBST.java
+++ MOOCTextEditor/src/spelling/DictionaryBST.java
@@ -9,9 +9,14 @@ import java.util.TreeSet;
 public class DictionaryBST implements Dictionary 
 {
    private TreeSet<String> dict;
+   private int size;
 	
     // TODO: Implement the dictionary interface using a TreeSet.  
  	// You'll need a constructor here
+   public DictionaryBST()	{
+	   this.dict = new TreeSet<String>();
+	   this.size = 0;
+   }
 	
     
     /** Add this word to the dictionary.  Convert it to lowercase first
@@ -21,7 +26,12 @@ public class DictionaryBST implements Dictionary
      * (it wasn't already there). */
     public boolean addWord(String word) {
     	// TODO: Implement this method
-        return false;
+    	word = word.toLowerCase();
+    	if (this.isWord(word))
+    		return false;
+    	this.dict.add(word);
+    	this.size++;
+    	return true;
     }
 
 
@@ -29,12 +39,14 @@ public class DictionaryBST implements Dictionary
     public int size()
     {
     	// TODO: Implement this method
-        return 0;
+        return this.size;
     }
 
     /** Is this a word according to this dictionary? */
     public boolean isWord(String s) {
     	//TODO: Implement this method
+    	if (this.dict.contains(s.toLowerCase()))
+    		return true;
         return false;
     }
 
